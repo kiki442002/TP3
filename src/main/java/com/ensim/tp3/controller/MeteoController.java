6ed53f290b1ea17b9962820e91fe8fab253c207e
PICOT Killian <picot.killian@outlook.fr>
2023-11-29 14:57:09 +0100

    ajout nouveau controller + récupération donnée POST

--- src/main/java/com/ensim/tp3/controller/MeteoController.java
+++ src/main/java/com/ensim/tp3/controller/MeteoController.java
@@ -5,16 +5,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-//@Controller
-public class AddressController {
-
+@Controller
+public class MeteoController {
     @Autowired
     AddressRepository addressRepository;
+
+    @PostMapping ("/meteo")
+    public String showMeteo(@RequestParam(name="address", required=true) String adressPOST, Model model) {
+        model.addAttribute("address", adressPOST);
+        return "meteo";
+    }
 
     @GetMapping("/addresses")
     public String showAddresses(Model model) {
         model.addAttribute("allAddresses", addressRepository.findAll());
-        return "addresses";
+        return "adresseMeteo";
     }
 }
+
