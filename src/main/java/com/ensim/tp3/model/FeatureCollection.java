ea3b498271fe8f0bbcdbfbe244a52591872cf874
PICOT Killian <picot.killian@outlook.fr>
2023-11-29 15:49:17 +0100

    création du record pour l'api de etalab

---
+++ src/main/java/com/ensim/tp3/model/FeatureCollection.java
@@ -0,0 +1,20 @@
+package com.ensim.tp3.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record FeatureCollection(@JsonProperty("features") Feature[] features){
+    private record Feature(@JsonProperty("geometry") Geometry geometry){
+        private record Geometry(@JsonProperty("coordinates") double[] coordinates){}
+
+    }
+
+    public String getAPICoordinates(){
+        return features[0].geometry.coordinates[0] + "%2C" + features[0].geometry.coordinates[1];
+    }
+
+
+}
+
