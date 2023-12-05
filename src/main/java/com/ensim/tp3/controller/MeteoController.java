c2780d8176c9cacc340b9b6e5217b420900a743b
PICOT Killian <picot.killian@outlook.fr>
2023-12-05 20:09:06 +0100

    gestion des controlleurs

--- src/main/java/com/ensim/tp3/controller/MeteoController.java
+++ src/main/java/com/ensim/tp3/controller/MeteoController.java
@@ -1,9 +1,7 @@
 package com.ensim.tp3.controller;
 
-import com.ensim.tp3.model.AddressRepository;
 import com.ensim.tp3.model.FeatureCollection;
 import com.ensim.tp3.model.WeatherData;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +11,10 @@ import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class MeteoController {
-    @Autowired
-    AddressRepository addressRepository;
 
+    private static final String TOKEN = "ce38c3036f77ec645780dcd805a14928edc7d855c481f9fd8f5516dd24057bc3";
     @PostMapping ("/meteo")
-    public String showMeteo(@RequestParam(name="address", required=true) String addressPOST, Model model, RestTemplate restTemplate) {
+    public String showMeteo(@RequestParam(name="address", required=true) String addressPOST, @RequestParam(name="day", required=true) String day, Model model, RestTemplate restTemplate) {
         FeatureCollection featureCollection = restTemplate.getForObject(
                 "https://api-adresse.data.gouv.fr/search/?limit=1&q="+addressPOST, FeatureCollection.class);
         if (featureCollection == null) {
@@ -25,17 +22,19 @@ public class MeteoController {
         }
 
         WeatherData wheatherData = restTemplate.getForObject(
-                "https://api.meteo-concept.com/api/forecast/daily?world=false&start=0&end=0&token=ce38c3036f77ec645780dcd805a14928edc7d855c481f9fd8f5516dd24057bc3&latlong="+featureCollection.getAPICoordinates()
+                "https://api.meteo-concept.com/api/forecast/daily?world=false&start=0&end="+day+
+                        "&token="+TOKEN+"&latlong="+featureCollection.getAPICoordinates()
                 , WeatherData.class);
 
-        model.addAttribute("address", wheatherData.forecast()[0].day());
-
+        if (wheatherData == null) {
+            return "error";
+        }
+        model.addAttribute("Allforecast", wheatherData.forecast());
         return "meteo";
     }
 
-    @GetMapping("/addresses")
-    public String showAddresses(Model model) {
-        model.addAttribute("allAddresses", addressRepository.findAll());
+    @GetMapping("/MeteoAdresse")
+    public String showMeteoAdresse() {
         return "adresseMeteo";
     }
 }
